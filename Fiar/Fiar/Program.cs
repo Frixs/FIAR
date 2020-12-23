b4d2865db1abaf0b1544da768bb111bb6061f0ee
Frixs <17912365+Frixs@users.noreply.github.com>
2020-12-23 18:16:24 +0100

    Base structure

---
+++ Fiar/Fiar/Program.cs
@@ -0,0 +1,28 @@
+// Task: https://github.com/osvetlik/pia2020/tree/master/semester-project
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
