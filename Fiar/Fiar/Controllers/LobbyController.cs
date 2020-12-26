03ceaf240f8dabe2f73ef379ff758e96f195067e
Frixs <17912365+Frixs@users.noreply.github.com>
2020-12-26 20:45:33 +0100

    Added lobby controller

---
+++ Fiar/Fiar/Controllers/LobbyController.cs
@@ -0,0 +1,88 @@
+﻿using Ixs.DNA;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Fiar
+{
+    /// <summary>
+    /// Manages the game lobby
+    /// </summary>
+    [Authorize(Roles = RoleNames.Player)]
+    [Route("lobby")]
+    public class LobbyController : Controller
+    {
+        #region Protected Members
+
+        /// <summary>
+        /// The scoped Application context
+        /// </summary>
+        protected ApplicationDbContext mContext;
+
+        /// <summary>
+        /// The manager for handling user creation, deletion, searching, roles, etc.
+        /// </summary>
+        protected UserManager<ApplicationUser> mUserManager;
+
+        /// <summary>
+        /// The manager for handling signing in and out for our users
+        /// </summary>
+        protected SignInManager<ApplicationUser> mSignInManager;
+
+        /// <summary>
+        /// The manager for handling user roles
+        /// </summary>
+        protected RoleManager<IdentityRole> mRoleManager;
+
+        /// <summary>
+        /// Injection - <inheritdoc cref="ILogger"/>
+        /// </summary>
+        private readonly ILogger mLogger;
+
+        /// <summary>
+        /// Injection - <inheritdoc cref="IConfigBox"/>
+        /// </summary>
+        private readonly IConfigBox mConfigBox;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="context">The injected context</param>
+        /// <param name="userManager">The Identity user manager</param>
+        /// <param name="signInManager">The Identity sign in manager</param>
+        /// <param name="roleManager">The Identity role manager</param>
+        public LobbyController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager, IConfigBox configBox)
+        {
+            mContext = context;
+            mUserManager = userManager;
+            mSignInManager = signInManager;
+            mRoleManager = roleManager;
+            mConfigBox = configBox ?? throw new ArgumentNullException(nameof(configBox));
+            mLogger = FrameworkDI.Logger ?? throw new ArgumentNullException(nameof(mLogger));
+        }
+
+        #endregion
+
+        #region Pages
+
+        /// <summary>
+        /// Base page (index)
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Requests
+
+        #endregion
+    }
+}
